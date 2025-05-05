5129127417d8b3a1f6b376310cd66f68bfa73687
CRMLeadUP <juniorzinho11@gmail.com>
2025-05-04 21:35:36 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: "/static/",  // Necessário para garantir que os arquivos JS/CSS sejam encontrados corretamente
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: "assets", // Coloca arquivos estáticos na pasta /static/assets/
   },
   resolve: {
     alias: {
